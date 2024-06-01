524a2bbbc32cbaf94f9cc96f9174818471ac4248
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-06-01 19:00:43 +0200

    Replace double quotes by single quotes prettier.

--- src/default-travel-list.js
+++ src/default-travel-list.js
@@ -1,23 +1,23 @@
 export const DEFAULT_TRAVEL_LIST = [
   {
-    name: "General",
+    name: 'General',
     items: [
-      { name: "Zapatillas de andar por casa", checked: false },
-      { name: "Pijama", checked: false },
+      { name: 'Zapatillas de andar por casa', checked: false },
+      { name: 'Pijama', checked: false },
     ],
   },
   {
-    name: "Montaña",
-    items: [{ name: "Chubasquero", checked: false }],
+    name: 'Montaña',
+    items: [{ name: 'Chubasquero', checked: false }],
   },
   {
-    name: "Neceser",
+    name: 'Neceser',
     items: [
-      { name: "Cepillo de dientes", checked: false },
-      { name: "Esponja, jabón, gel y shampoo", checked: false },
-      { name: "Cortauñas", checked: false },
-      { name: "Colonia", checked: false },
-      { name: "Máquina de afeitar", checked: false },
+      { name: 'Cepillo de dientes', checked: false },
+      { name: 'Esponja, jabón, gel y shampoo', checked: false },
+      { name: 'Cortauñas', checked: false },
+      { name: 'Colonia', checked: false },
+      { name: 'Máquina de afeitar', checked: false },
     ],
   },
 ];
